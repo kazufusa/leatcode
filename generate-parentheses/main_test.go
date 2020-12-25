63a64536582175fbe0b05ffc81c87dbed399e7ef
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2020-12-26 00:14:19 +0900

    add generate parentheses

--- generate-parentheses/main_test.go
+++ generate-parentheses/main_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"sort"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,12 +27,43 @@ func Test_1(t *testing.T) {
 }
 
 func generateParenthesis(n int) []string {
-	var patterns [][]int
-	pattern := make([]int, n)
-	for {
-		patterns = append(patterns, pattern)
+	if n == 1 {
+		return []string{"()"}
 	}
 	ret := []string{}
+	graph := make([]int, n)
+
+LOOP:
+	for {
+		for i := n - 1; i >= 0; i-- {
+			if graph[i] > i {
+				if i == 1 {
+					break LOOP
+				}
+				graph[i-1]++
+				for j := i; j < n; j++ {
+					graph[j] = graph[i-1]
+				}
+			}
+		}
+		ps := make([]string, n)
+
+		for i := n - 1; i >= 0; i-- {
+			if ps[i] == "" {
+				ps[i] = "()"
+			} else {
+				ps[i] = "(" + ps[i] + ")"
+			}
+			if graph[i] != 0 {
+				ps[graph[i]-1] += ps[i]
+				ps[i] = ""
+			}
+		}
+		ret = append(ret, strings.Join(ps, ""))
+
+		graph[n-1]++
+	}
+
 	sort.Strings(ret)
 	return ret
 }
